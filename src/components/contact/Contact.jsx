c064ccd13630e64a60a8c74c84b6f760110a64a7
jean-yves1994 <roidalas@gmail.com>
2022-11-07 22:09:24 +0200

    experience about contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>roidalas@gmail.com</h5>
+            <a href="">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine/>
+            <h4>Messenger</h4>
+            <h5>Yves king Dala</h5>
+            <a href="">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp/>
+            <h4>Whatsapp</h4>
+            <h5>+250785252225</h5>
+            <a href="">Send a message</a>
+          </article>
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,96 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>Skills I Have</h5>
+      <h2>Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>Javascript</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>React JS</h4>
+                <small className="text-light">Intermaediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>Laravel</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>Dart</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icons"/>
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Intermaediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
