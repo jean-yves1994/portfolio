3efcb5d061da9b8a064a4f7402124438690f6fc2
jean-yves1994 <roidalas@gmail.com>
2022-11-07 13:44:00 +0200

    link to section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import './about.css';
-const About = props => {
-  return (
-    <div>About</div>
-  )
-}
+import React from "react";
+import "./about.css";
+const About = (props) => {
+  return <section id="about">About</section>;
+};
 
-
-export default About
+export default About;
